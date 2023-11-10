d737a79219d029474a205b902243d7144a23b309
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-11-10 20:03:28 +0900

    Update

--- src/app/blanks/blanksSerivce.js
+++ src/app/blanks/blanksSerivce.js
@@ -3,7 +3,7 @@ export const BlankService = {
     getBlanks : async(number,text)=>{
         const result = await axios({
             method: 'POST',
-            url: 'http://127.0.0.1:5000/main/extract-keywords', //서버 주소,
+            url: 'http://127.0.0.1:5000/main/extract_keywords', //서버 주소,
             headers:{
                 "Content-Type" : "application/json"
             },
--- src/app/question/questionService.js
+++ src/app/question/questionService.js
@@ -5,22 +5,22 @@ import { response } from "../../../config/response";
 import baseResponse from "../../../config/baseResponseStatus";
 export const QuestionService = {
     createQuestions : async(body)=>{
-        console.log(body);
         const text = body.text;
+        //const keyWords = body.keyWords;
         const blankNo = body.BlankNo;
-        const subNo = body.SubNo;
+        //const subNo = body.SubNo;
         const keyWords = await BlankService.getBlanks(blankNo,text);
         //const questions = await SubjectiveService.getQuestions(subNo,text);
         const modifiedText = replaceKeywordsWithBlank(text, keyWords);
         console.log(modifiedText);
         //console.log(questions);
-        return response(baseResponse.SUCCESS,modifiedText);
+        return response(baseResponse.SUCCESS,{modifiedText,keyWords});
     }
 }
 
 function replaceKeywordsWithBlank(text, keywords) {
     const modifiedText = keywords.reduce((modified, keyword, index) => {
-        const placeholder = `<input type="text" name="blank${index + 1}" />`; // Generate placeholder like 'Blank1', 'Blank2', ...
+        const placeholder = `<blank${index + 1}>`; // Generate placeholder like 'Blank1', 'Blank2', ...
         const regex = new RegExp(keyword, 'gi');
         return modified.replace(regex, placeholder);
     }, text);
